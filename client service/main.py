fb599090136013648a9e2efb5e687140685e85b7
Heinz217 <160035856+Heinz217@users.noreply.github.com>
2024-03-27 20:07:14 +0800

    Update main.py

--- client service/main.py
+++ client service/main.py
@@ -13,9 +13,8 @@ app = FastAPI()
 message_queue = Queue()
 
 class Message(BaseModel):
-    host:str
-    port:str
-    text:str
+    id: str
+    text: str
 
 def process_message(message: Message):
     # 在后台处理消息
@@ -24,27 +23,10 @@ def process_message(message: Message):
     text=tools.message()
     # 实际上是：text=tools.message(text)
     message_now={
-        "host":message.host,
-        "port":message.port,
+        "id": message.id,
         "text":text
     }
     message_queue.put(message_now)
-
-def send_message_to_user(message: Message, request:Request):
-    # 从队列中获取消息并发送给用户
-    processed_message = message_queue.get()
-
-    # 这里必须改，url是为了测试才这样写的
-    user_endpoint = f"http://{processed_message['host']}:{processed_message['port']}/ask/"
-    # user_endpoint = f"http://{request.client.host}:{request.client.port}/ask/"
-    headers = {'Connection': 'close'}
-    proxies = {"http": None, "https": None}
-    print(type(processed_message))
-    processed_message = {"host": "192.168.177.103",
-                         "port": "17327",
-                         "text": "Hello, World!"
-    }
-    response = requests.post(user_endpoint, json=processed_message, proxies=proxies)
 
 @app.post("/ask/")
 async def send_message(message: Message, background_tasks: BackgroundTasks):
@@ -54,15 +36,17 @@ async def send_message(message: Message, background_tasks: BackgroundTasks):
     return {"chat_system": "Message received."}
 
 @app.get("/answer/")
-async def process_queue(background_tasks: BackgroundTasks):
-    # 后台任务，处理队列中的消息并发送给用户
+async def process_queue():
     if not message_queue.empty():
-        background_tasks.add_task(send_message_to_user, None)
-        return {"chat_system": "Messages are sent."}
+        message = message_queue.get()
+        text = message.text
+        return {
+        "id": message.id,
+        "text": text
+    }
     else:
         return {"chat_system": "No message now."}
 
 
 if __name__=='__main__':
     uvicorn.run("main:app",host="192.168.177.103",port=17327,debug=True,reload=True)
-
